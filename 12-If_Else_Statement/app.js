e2f157f914926aa5d75b4250a79a2da495b6723e
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-04-28 09:54:36 +0500

    Add files via upload

---
+++ 12-If_Else_Statement/app.js
@@ -0,0 +1,50 @@
+
+// var age = 19
+
+// if(age >= 19){
+// console.log('Eligible')
+// }else{
+//     console.log("Not Eligible")
+// }
+
+// var laptop = prompt("konsa Laptop Mil Raya Hai!")
+
+// if(laptop === "hp"){
+
+// console.log("Buy kr ly")
+
+// }else if(laptop === "dell"){
+
+//     console.log("Purchase Kr ly Bhai ")
+
+// }else if(laptop === "lenovo"){
+
+//     console.log("PriceKyaHai")
+   
+    
+// }else if( laptop === "apple"){
+
+//     console.log("Paisa Hai Kya")
+// }
+
+var language = prompt("Enter Your Language");
+
+if(language === "javascript"){
+    var city = prompt("Enter Your City");
+if(city === "karachi"){
+    var exp = prompt("Enter Your Experince");
+if(exp === "2years"){
+      console.log("Congratulation,You Hired!")
+
+    }else{
+      console.log("Sorry Reject You"); } 
+    } 
+    else{
+        console.log("Come To Karachi")}
+    }
+     else{
+       console.log(" Study JavaScript ")
+    }
+
+
+
